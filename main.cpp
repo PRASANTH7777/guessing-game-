e54c723f46a83f697c60eaaaf7e252214fa47ebe
prasanth <47972446+PRASANTH7777@users.noreply.github.com>
2021-03-11 12:57:42 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int secretNum = 7;
+    int guess;
+
+    while(secretNum != guess){
+        cout <<"Enter guess if not correct try again :): ";
+        cin>> guess;
+
+    }
+    cout << "You win !";
+
+
+    return 0;
+}
